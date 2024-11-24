8fdeadfee1ccd31a7179ffc223cb4810a56956b6
Dan (Laptop) <danshute@hotmail.co.uk>
2024-11-24 16:29:05 +0100

    Adds two methods to the database_utils.py script. These methods read the YAML credentials and initialise a db engine

--- database_utils.py
+++ database_utils.py
@@ -1,6 +1,19 @@
 # This script will contain the class DatabaseConnector.
 # It will be used to connect with and upload to the database
 
+import yaml
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+
 class DatabaseConnector:
-    def __init__(self,credentials):
-        self.credentials = credentials
+    
+    def read_db_creds(self):
+        with open('db_creds.yaml', 'r') as f:
+            data = yaml.load(f, Loader=yaml.SafeLoader)
+        return data
+    
+    def init_db_engine(self):
+        creds = self.read_db_creds()
+        engine = create_engine("postgresql://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}".format(**creds), echo = True)
+        return engine
