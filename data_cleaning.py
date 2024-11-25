2bda1dc311242a1d63066a6cf7b786a0b7294755
Dan (Laptop) <danshute@hotmail.co.uk>
2024-11-25 16:04:08 +0100

    Adds methods for reading database tables into the data_extraction class. Adds a method to clean a dataframe in the DataCleaning class

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,12 @@
 # This script will contain a class DataCleaning with methods to clean data from each of the sources
+import pandas as pd
+
+class DataCleaning:
+    def __init__(self):
+        pass
+
+    def clean_user_data(self, df):
+        df = df.replace('NULL', None)
+        df = df.dropna(how = 'any', axis = 0)
+        df['join_date'] = pd.to_datetime(df['join_date'], format = 'mixed', yearfirst = True, errors='coerce')
+        return df
--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,23 @@
 # This script will contain the methods for extracting the data from various sources such as CSV files, an API and an S3 Bucket.
+from sqlalchemy import inspect as insp
+import pandas as pd
+
+
 
 class DataExtractor:
-    def __init__(self, source):
-        self.source = source
+    def __init__(self):
+        pass
+
+    def list_db_tables(self, engine):
+        inspector = insp(engine)
+        return inspector.get_table_names()
+    
+    def read_rds_table(self, db_conn, table_name):
+        engine = db_conn.init_db_engine()
+        query = f"SELECT * FROM {table_name}"
+        df = pd.read_sql(query, engine)
+        return df
+
+
 
     
