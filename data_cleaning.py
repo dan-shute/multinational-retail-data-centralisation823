753f4f469093fdffd304e2f57be660a68e77405f
Dan (Laptop) <danshute@hotmail.co.uk>
2024-11-29 15:00:01 +0100

    Adds cleaning rules to the clean_card_data method.

--- data_cleaning.py
+++ data_cleaning.py
@@ -11,4 +11,14 @@ class DataCleaning:
         df = df.dropna(how = 'any', axis = 0)
         return df
     
+    def clean_card_data(self, df):
+        df = df.replace('NULL', None)
+        df['card_number'] = df['card_number'].astype(str).str.extract(r'(\d+\.*\d*)')
+        df = df.drop_duplicates(subset= 'card_number', keep= 'last')
+        df = df[pd.to_numeric(df['card_number'], errors= 'coerce').notnull()]
+        df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], format = 'mixed', yearfirst = True, errors='coerce')
+        df = df.dropna(how = 'any', axis = 0)
+        return df
+        
+    
     
